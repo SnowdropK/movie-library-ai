09d02d33dc4d2bc094c7095da5b333e51278382a
chenxia <snowdropsorange@gmail.com>
2025-09-03 04:18:11 +0800

    优化

--- src/views/Poster/data.ts
+++ src/views/Poster/data.ts
@@ -2,36 +2,36 @@
 import bopuAi from '@/assets/postersAi/bopu.jpg'
 import bopuAi2 from '@/assets/postersAi/bopu2.jpg'
 import bopuAi3 from '@/assets/postersAi/bopu3.jpg'
-import bopuAi4 from '@/assets/postersAi/bopu4.jpg'
-import bopuAi5 from '@/assets/postersAi/bopu5.jpg'
+// import bopuAi4 from '@/assets/postersAi/bopu4.jpg'
+// import bopuAi5 from '@/assets/postersAi/bopu5.jpg'
 
 import guzhangAi from '@/assets/postersAi/guzhang.jpg'
 import guzhangAi2 from '@/assets/postersAi/guzhang2.jpg'
 import guzhangAi3 from '@/assets/postersAi/guzhang3.jpg'
-import guzhangAi4 from '@/assets/postersAi/guzhang4.jpg'
-import guzhangAi5 from '@/assets/postersAi/guzhang5.jpg'
-import guzhangAi6 from '@/assets/postersAi/guzhang6.jpg'
+// import guzhangAi4 from '@/assets/postersAi/guzhang4.jpg'
+// import guzhangAi5 from '@/assets/postersAi/guzhang5.jpg'
+// import guzhangAi6 from '@/assets/postersAi/guzhang6.jpg'
 
 import suanxingAi from '@/assets/postersAi/suanxing.jpg'
 import suanxingAi2 from '@/assets/postersAi/suanxing2.jpg'
 import suanxingAi3 from '@/assets/postersAi/suanxing3.jpg'
 import suanxingAi4 from '@/assets/postersAi/suanxing4.jpg'
-import suanxingAi5 from '@/assets/postersAi/suanxing5.jpg'
-import suanxingAi6 from '@/assets/postersAi/suanxing6.jpg'
-import suanxingAi7 from '@/assets/postersAi/suanxing7.jpg'
-import suanxingAi8 from '@/assets/postersAi/suanxing8.jpg'
+// import suanxingAi5 from '@/assets/postersAi/suanxing5.jpg'
+// import suanxingAi6 from '@/assets/postersAi/suanxing6.jpg'
+// import suanxingAi7 from '@/assets/postersAi/suanxing7.jpg'
+// import suanxingAi8 from '@/assets/postersAi/suanxing8.jpg'
 
 import xiangsuAi from '@/assets/postersAi/xiangsu.jpg'
 import xiangsuAi2 from '@/assets/postersAi/xiangsu2.jpg'
 import xiangsuAi3 from '@/assets/postersAi/xiangsu3.jpg'
 import xiangsuAi4 from '@/assets/postersAi/xiangsu4.jpg'
-import xiangsuAi5 from '@/assets/postersAi/xiangsu5.jpg'
-import xiangsuAi6 from '@/assets/postersAi/xiangsu6.jpg'
+// import xiangsuAi5 from '@/assets/postersAi/xiangsu5.jpg'
+// import xiangsuAi6 from '@/assets/postersAi/xiangsu6.jpg'
 
 import jianzhiAi from '@/assets/postersAi/jianzhi.jpg'
 import jianzhiAi2 from '@/assets/postersAi/jianzhi2.jpg'
 import jianzhiAi3 from '@/assets/postersAi/jianzhi3.jpg'
-import jianzhiAi4 from '@/assets/postersAi/jianzhi4.jpg'
+// import jianzhiAi4 from '@/assets/postersAi/jianzhi4.jpg'
 
 import weiduoliyaAi from '@/assets/postersAi/weiduoliya.jpg'
 import weiduoliyaAi2 from '@/assets/postersAi/weiduoliya2.jpg'
@@ -43,17 +43,17 @@ const bopu: any[] = [
   bopuAi, 
   bopuAi2,
   bopuAi3,
-  bopuAi4,
-  bopuAi5
+  // bopuAi4,
+  // bopuAi5
 ]
 // 故障
 const guzhang: any[] = [
   guzhangAi,
   guzhangAi2,
   guzhangAi3,
-  guzhangAi4,
-  guzhangAi5,
-  guzhangAi6
+  // guzhangAi4,
+  // guzhangAi5,
+  // guzhangAi6
 ]
 // 酸性
 const suanxing: any[] = [
@@ -61,10 +61,10 @@ const suanxing: any[] = [
   suanxingAi2,
   suanxingAi3,
   suanxingAi4,
-  suanxingAi5,
-  suanxingAi6,
-  suanxingAi7,
-  suanxingAi8
+  // suanxingAi5,
+  // suanxingAi6,
+  // suanxingAi7,
+  // suanxingAi8
 ]
 // 像素
 const xiangsu: any[] = [
@@ -72,15 +72,15 @@ const xiangsu: any[] = [
   xiangsuAi2,
   xiangsuAi3,
   xiangsuAi4,
-  xiangsuAi5,
-  xiangsuAi6
+  // xiangsuAi5,
+  // xiangsuAi6
 ]
 // 剪纸
 const jianzhi: any[] = [
   jianzhiAi,
   jianzhiAi2,
   jianzhiAi3,
-  jianzhiAi4
+  // jianzhiAi4
 ]
 // 维多利亚
 const weiduoliya: any[] = [
